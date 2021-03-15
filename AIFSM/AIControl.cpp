5e4ea14b7229c3d0bc69bec75a293effae47fc14
cxvisionai <37944416+cxvisionai@users.noreply.github.com>
2021-03-15 09:00:52 +0000

    Create AIControl.cpp

---
+++ AIFSM/AIControl.cpp
@@ -0,0 +1,18 @@
+#include "AIControl.h"
+//#include "Ship.h"
+
+//---------------------------------------------------------
+AIControl::AIControl():
+Control()
+{
+
+	m_type = CONTROLTYPE_AI;
+
+	//m_debugTxt = new char[30];
+   // m_target   = NULL;
+}
+
+void AIControl::Update(float dt)
+{
+	//put AI code here
+}
