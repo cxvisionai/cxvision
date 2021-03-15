dc40ccb38ccf33501961b669dbd4ae17adb69431
cxvisionai <37944416+cxvisionai@users.noreply.github.com>
2021-03-15 09:53:23 +0000

    Create diagramtextitem.h

---
+++ diagram/diagramtextitem.h
@@ -0,0 +1,38 @@
+
+#ifndef DIAGRAMTEXTITEM_H
+#define DIAGRAMTEXTITEM_H
+
+#include <QGraphicsTextItem>
+#include <QPen>
+
+QT_BEGIN_NAMESPACE
+class QFocusEvent;
+class QGraphicsItem;
+class QGraphicsScene;
+class QGraphicsSceneMouseEvent;
+QT_END_NAMESPACE
+
+//! [0]
+class DiagramTextItem : public QGraphicsTextItem
+{
+    Q_OBJECT
+
+public:
+    enum { Type = UserType + 3 };
+
+    DiagramTextItem(QGraphicsItem *parent = 0);
+
+    int type() const override { return Type; }
+
+signals:
+    void lostFocus(DiagramTextItem *item);
+    void selectedChange(QGraphicsItem *item);
+
+protected:
+    QVariant itemChange(GraphicsItemChange change, const QVariant &value) override;
+    void focusOutEvent(QFocusEvent *event) override;
+    void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event) override;
+};
+//! [0]
+
+#endif // DIAGRAMTEXTITEM_H
