75bbeaf9f221798c78f726e23da48921f7c9c864
cxvisionai <37944416+cxvisionai@users.noreply.github.com>
2021-03-15 09:40:33 +0000

    Create arrow.cpp

---
+++ diagram/arrow.cpp
@@ -0,0 +1,120 @@
+
+
+#include "arrow.h"
+
+#include <qmath.h>
+#include <QPen>
+#include <QPainter>
+
+//! [0]
+Arrow::Arrow(DiagramItem *startItem, DiagramItem *endItem, QGraphicsItem *parent)
+    : QGraphicsLineItem(parent)
+{
+    myStartItem = startItem;
+    myEndItem = endItem;
+    setFlag(QGraphicsItem::ItemIsSelectable, true);
+    myColor = Qt::black;
+    setPen(QPen(myColor, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+}
+//! [0]
+
+//! [1]
+QRectF Arrow::boundingRect() const
+{
+    qreal extra = (pen().width() + 20) / 2.0;
+
+    return QRectF(line().p1(), QSizeF(line().p2().x() - line().p1().x(),
+                                      line().p2().y() - line().p1().y()))
+        .normalized()
+        .adjusted(-extra, -extra, extra, extra);
+}
+//! [1]
+
+//! [2]
+QPainterPath Arrow::shape() const
+{
+    QPainterPath path = QGraphicsLineItem::shape();
+    path.addPolygon(arrowHead);
+    return path;
+}
+//! [2]
+
+//! [3]
+void Arrow::updatePosition()
+{
+    QLineF line(mapFromItem(myStartItem, 0, 0), mapFromItem(myEndItem, 0, 0));
+    setLine(line);
+}
+//! [3]
+
+//! [4]
+void Arrow::paint(QPainter *painter, const QStyleOptionGraphicsItem *,
+          QWidget *)
+{
+    if (myStartItem->collidesWithItem(myEndItem))
+        return;
+
+    QPen myPen = pen();
+    myPen.setColor(myColor);
+    qreal arrowSize = 20;
+    painter->setPen(myPen);
+    painter->setBrush(myColor);
+//! [4] //! [5]
+
+    QLineF centerLine(myStartItem->pos(), myEndItem->pos());
+    QPolygonF endPolygon = myEndItem->polygon();
+    QPointF p1 = endPolygon.first() + myEndItem->pos();
+    QPointF p2;
+    QPointF intersectPoint;
+    QLineF polyLine;
+    for (int i = 1; i < endPolygon.count(); ++i) {
+        p2 = endPolygon.at(i) + myEndItem->pos();
+        polyLine = QLineF(p1, p2);
+        QLineF::IntersectType intersectType =
+            polyLine.intersect(centerLine, &intersectPoint);
+        if (intersectType == QLineF::BoundedIntersection)
+            break;
+        p1 = p2;
+    }
+
+    setLine(QLineF(intersectPoint, myStartItem->pos()));
+//! [5] //! [6]
+
+    double angle = std::atan2(-line().dy(), line().dx());
+
+    QPointF arrowP1 = line().p1() + QPointF(sin(angle + M_PI / 3) * arrowSize,
+                                    cos(angle + M_PI / 3) * arrowSize);
+    QPointF arrowP2 = line().p1() + QPointF(sin(angle + M_PI - M_PI / 3) * arrowSize,
+                                    cos(angle + M_PI - M_PI / 3) * arrowSize);
+
+    arrowHead.clear();
+    arrowHead << line().p1() << arrowP1 << arrowP2;
+//! [6] //! [7]
+     painter->drawLine(line());
+//QRect rect1(QPoint(line().x1(),line().y1()),QPoint(line().x2(),line().y2()));
+//int startAngle = 20 * 16;
+//int arcLength = 120 * 16;
+//painter->drawArc(rect1, startAngle, arcLength);
+
+    //  QPainterPath path;
+    //  path.moveTo(line().x1(), line().y1());
+    //  path.cubicTo(line().x1(), line().y1(), (line().x1()+line().x2())/2, (line().y1()+line().y2())/2,line().x2(), line().y2());
+    //  painter->drawPath(path);
+    painter->drawPolygon(arrowHead);
+    if (isSelected())
+    {
+        painter->setPen(QPen(myColor, 1, Qt::DashLine));
+        QLineF myLine = line();
+        myLine.translate(0, 14.0);
+        painter->drawLine(myLine);
+        myLine.translate(0,-18.0);
+        painter->drawLine(myLine);
+    }
+}
+//! [7]
+void Arrow::SetDiagramModelConnectName(const QString strmodelname)
+{
+
+    setData(2,strmodelname);
+    m_strModelConnectname = strmodelname;
+}
